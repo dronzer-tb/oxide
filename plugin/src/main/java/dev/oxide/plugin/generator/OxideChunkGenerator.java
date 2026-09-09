d0664928716182bf29c87aa6b1d73fe4676d11a5
x00f8 <dronzertb@gmail.com>
2026-09-09 05:50:06 +0000

    revert(generator): re-enable shouldGenerateStructures() until native jigsaw pipeline is fully ported

--- plugin/src/main/java/dev/oxide/plugin/generator/OxideChunkGenerator.java
+++ plugin/src/main/java/dev/oxide/plugin/generator/OxideChunkGenerator.java
@@ -249,7 +249,7 @@ public final class OxideChunkGenerator extends ChunkGenerator {
 
     @Override
     public boolean shouldGenerateStructures() {
-        return false;
+        return true;
     }
 
     @Override
